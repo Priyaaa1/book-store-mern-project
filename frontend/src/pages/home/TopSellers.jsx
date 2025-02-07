b2482bc283604eee0e3435abe835ab9ab7ac8689
Priya <priyamaheshwari183@gmail.com>
2025-02-07 16:12:33 +0530

    added top sellers section

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,4 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import BookCard from '../books/BookCard';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import required modules
+import { Navigation, Pagination } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const categories = ["Choose a genre", "Business", "Fiction", "Horror", "Adventure"]
 
@@ -30,11 +42,43 @@ const TopSellers = () => {
             </select>
         </div>
 
+        <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          }
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
         {
-            filteredBooks.map((book, index) => (
-                <div>{book.title}</div>
+            filteredBooks.length > 0 && filteredBooks.map((book, index) => (
+                <SwiperSlide key={index}>
+                    <BookCard book={book} />
+                </SwiperSlide>
             ))
         }
+        
+        
+      </Swiper>
+
+        
     </div>
   )
 }
