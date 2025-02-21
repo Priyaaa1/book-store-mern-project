e4eee04ea41b9d0db8d9bf083cc1ebee2d2d0f41
Priya <priyamaheshwari183@gmail.com>
2025-02-22 01:49:44 +0530

    created login page

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
+        <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+            <h2 className='text-xl font-semibold mb-4'>Please Login</h2>
+
+            <form>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>Email</label>
+                    <input type='email' name='email' id='email' placeholder='Email address' className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'></input>
+                </div>
+
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>Password</label>
+                    <input type='password' name='password' id='password' placeholder='Password' className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'></input>
+                </div>
+
+                <div className=''>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded focus:outline-none'>Login</button>
+                </div>
+            </form>
+            <p className='align-baseline font-medium mt-4 text-sm'>Do not have an account? <Link to="/register" className='text-blue-500 hover:text-blue-700'>Register here</Link></p>
+        </div>
+    </div>
+  )
+}
+
+export default Login
