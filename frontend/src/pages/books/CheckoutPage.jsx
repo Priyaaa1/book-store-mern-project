7683601af95c80ef14ce568352165f4d059472bb
Priya <priyamaheshwari183@gmail.com>
2025-03-15 00:23:42 +0530

    fixed checkout form

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -15,7 +15,23 @@ const CheckoutPage = () => {
     } = useForm();
 
     const [isChecked, setIsChecked] = useState(false);
-    const onSubmit = (data) => console.log(data);
+
+    const onSubmit = (data) => {
+        const newOrder = {
+            name: data.name,
+            email: currentUser?.email,
+            address: {
+                city: data.city,
+                country: data.country,
+                state: data.state,
+                zipcode: data.zipcode
+            },
+            phone: data.phone,
+            productIds: cartItems.map(item => item?._id),
+            totalPrice: totalPrice,
+        }
+        console.log(newOrder)
+    }
 
   return (
     <section>
@@ -23,7 +39,7 @@ const CheckoutPage = () => {
             <div className="container max-w-screen-lg mx-auto">
                 <div>
                     <div>
-                        <h2 className="font-semibold text-xl text-gray-600 mb-2">Cash On Delevary</h2>
+                        <h2 className="font-semibold text-xl text-gray-600 mb-2">Cash On Delivery</h2>
                         <p className="text-gray-500 mb-2">Total Price: $0</p>
                         <p className="text-gray-500 mb-6">Items:0</p>
                     </div>
@@ -40,7 +56,7 @@ const CheckoutPage = () => {
                                 <div className="md:col-span-5">
                                     <label htmlFor="full_name">Full Name</label>
                                     <input
-                                       
+                                        {...register("name", { required: true })} 
                                         type="text" name="name" id="name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"  />
                                 </div>
 
@@ -70,7 +86,7 @@ const CheckoutPage = () => {
                                 <div className="md:col-span-2">
                                     <label htmlFor="city">City</label>
                                     <input
-                                       
+                                        {...register("city", { required: true })}
                                         type="text" name="city" id="city" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                                 </div>
 
@@ -78,7 +94,7 @@ const CheckoutPage = () => {
                                     <label htmlFor="country">Country / region</label>
                                     <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                                         <input
-                                           
+                                            {...register("country", { required: true })}
                                             name="country" id="country" placeholder="Country" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"  />
                                         <button tabIndex="-1" className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
                                             <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -96,7 +112,7 @@ const CheckoutPage = () => {
                                     <label htmlFor="state">State / province</label>
                                     <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                                         <input 
-                                        
+                                        {...register("state", { required: true })}
                                         name="state" id="state" placeholder="State" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"  />
                                         <button  className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
                                             <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -113,13 +129,14 @@ const CheckoutPage = () => {
                                 <div className="md:col-span-1">
                                     <label htmlFor="zipcode">Zipcode</label>
                                     <input 
-                                  
+                                    {...register("zipcode", { required: true })}
                                     type="text" name="zipcode" id="zipcode" className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                                 </div>
 
                                 <div className="md:col-span-5 mt-3">
                                     <div className="inline-flex items-center">
                                         <input 
+                                        onChange={(e) => setIsChecked(e.target.checked)}
                                         type="checkbox" name="billing_same" id="billing_same" className="form-checkbox" />
                                         <label htmlFor="billing_same" className="ml-2 ">I am aggree to the <Link className='underline underline-offset-2 text-blue-600'>Terms & Conditions</Link> and <Link className='underline underline-offset-2 text-blue-600'>Shoping Policy.</Link></label>
                                     </div>
